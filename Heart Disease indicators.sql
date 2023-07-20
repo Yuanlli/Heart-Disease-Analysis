2355649c4c79aea21e49ebaf593bfa346afb3ea7
Yuan <35889216+Yuanlli@users.noreply.github.com>
2023-07-21 00:26:16 +0800

    Update Heart Disease indicators.sql

--- Heart Disease indicators.sql
+++ Heart Disease indicators.sql
@@ -31,3 +31,14 @@ SELECT Count(HeartDisease)As TotalHeartDisease
 FROM dbo.heart_2020 
 WHERE HeartDisease =1 
 
+-- Determine the group with higher risks of heart problems, arranged by older age
+SELECT HeartDisease, Stroke, AgeCategory, Diabetic,
+CASE
+   WHEN HeartDisease = 1 AND Stroke = 1 THEN 'HighRisk'
+   WHEN Diabetic = 'Yes' THEN 'ChronicDisease'
+   ELSE 'Normal'
+END
+FROM dbo.heart_2020
+Order BY AgeCategory DESC
+
+
